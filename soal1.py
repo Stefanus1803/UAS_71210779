35fe2675ae062f53b8e1d91886e60af14be1db4a
Stefanus <93358497+Stefanus1803@users.noreply.github.com>
2023-01-03 14:51:02 +0700

    Program Penghitung Poin Keaktifan Mahasiswa

--- soal1.py
+++ soal1.py
@@ -1,86 +1,55 @@
-poinKeaktifan = {
+activity_points = {
     "Prestasi": 30,
     "Organisasi": 10,
     "Kepanitiaan": 5,
     "Rekognisi": 2
 }
 
-print("*"*6,"Kredit Keaktifan Mahasiswa","*"*6)
-print("(Students Activities Credit)")
-print("1. Menambahkan Kegiatan\n2. Menampilkan Jumlah Poin\n3. Keluar")
-print("-"*30)
-pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+total_points = 0
+activities = {}
 
-while pilihan == True:
-    if pilihan == 1:
-        kegiatan = str(input("Nama Kegiatan: "))
-        tanggal = str(input("Tanggal Kegiatan: "))
+def add_activity(activity, date, category):
+    global total_points
+    if activity.lower() in [a.lower() for a in activities]:
+        return "Kegiatan sudah pernah diinput. Tidak boleh double claim!"
+    else:
+        activities[activity] = [date, category, activity_points[category]]
+        total_points += activity_points[category]
+        return "\nKegiatan berhasil ditambahkan.\n"
+
+while True:
+    print('******* Kredit Keaktifan Mahasiswa ******\n(Student Activities Credit)')
+    print("1. Menambahkan Kegiatan")
+    print("2. Menampilkan Jumlah Poin")
+    print("3. Keluar")
+    print("-"*25)
+    choice = input("Silahkan masukan pilihan Anda: ")
+    
+    if choice == "1":
+        activity = input("Nama Kegiatan: ")
+        date = input("Tanggal Kegiatan: ")
         print("Pilihan Kategori Kegiatan:")
-        print(poinKeaktifan)
-        pilihanKategori = str(input("Masukkan Kategori Kegiatan: "))
-        print("Kategori berhasil ditambahkan.")
+        for category in activity_points:
+            print(f" - {category}")
 
-    if pilihan == 2:
+        valid_input = False
+        while not valid_input:
+            category = input("Masukan Kategori Kegiatan: ").title()
+            if category in activity_points:
+                valid_input = True
+            else:
+                print()
+        print(add_activity(activity, date, category))
+    elif choice == "2":
+        print("\nTotal Poin:")
+        print("Nama Kegiatan\tTanggal\tKategori\tPoin")
+        total_points = 0
+        for i, registered_activity in enumerate(activities, start=1):
+            print(f"{i}. {registered_activity}", end="\t")
+            print(*activities[registered_activity], sep="\t")
+            total_points += activities[registered_activity][-1]
+        print(f"JUMLAH TOTAL POIN\t: {total_points}")
         print()
-        print("-"*30)
-        print("Nama Kegiatan     ","Tanggal      ","Kategori      ","Poin      ")
-        
-        
-    if pilihan == 3:
+    else:
         print("Sistem Berhenti...")
         break
-
-
-
-
-
-
-
-#cadangan
-# activity_data = {}
-# activity_points = {
-#     "Prestasi": 30,
-#     "Organisasi": 10,
-#     "Kepanitiaan": 5,
-#     "Rekognisi": 2
-# }
-
-# def add_activity(date, category, points):
-#   if date in activity_data:
-#     print("Kegiatan sudah pernah diinput. Tidak boleh double claim!")
-#   else:
-#     activity_data[date] = (category, points)
-
-# print("*"*6,"Kredit Keaktifan Mahasiswa","*"*6)
-# print("(Students Activities Credit)")
-# print("1. Menambahkan Kegiatan\n2. Menampilkan Jumlah Poin\n3. Keluar")
-# print("-"*30)
-
-# while True:
-#   add_activity_choice = int(input("Silahkan masukkan pilihan Anda: "))
-#   if add_activity_choice == 1:
-#     activity_name = str(input("Nama Kegiatan: "))
-#     date = input("Tanggal kegiatan: ")
-#     print("Kategori kegiatan:")
-#     for category in activity_points:
-#         print(f" - {category}")
-#         category = input("Pilih kategori kegiatan: ")
-
-  
-#   if add_activity_choice == 3:
-#     print("Sistem Berhenti...")
-#     break
-
-#   date = input("Tanggal kegiatan: ")
-#   print("Kategori kegiatan:")
-#   for category in activity_points:
-#     print(f" - {category}")
-#   category = input("Pilih kategori kegiatan: ")
-
-#   # Menambahkan data kegiatan ke dictionary
-#   points = activity_points[category]
-#   add_activity(date, category, points)
-
-# #jumlah total poin
-# total_points = sum([points for _, (category, points) in activity_data.items()])
-# print(f"JUMLAH POIN TOTAL POIN   : {total_points}")
